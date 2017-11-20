803769c638c33e14f843c0cc5db2f6b3837aba02
Devin Pastoor <devin.pastoor@gmail.com>
2017-11-20 12:44:45 -0500

    add additional ggforce util reqs

--- R/ggforce-reqs.R
+++ R/ggforce-reqs.R
@@ -1,6 +1,32 @@
 # taken from ggforce directly to remove heavy dependency
 # chain ggforce introduces
 
+
+## utils
+
+globalVariables(
+  c(
+    '.',
+    'x',
+    'y'
+  )
+)
+
+`%||%` <- function(x, y) {
+  if (is.null(x)) y else x
+}
+
+is.waive <- function(x) inherits(x, 'waiver')
+
+`%|W|%` <- function(x, y) {
+  if (is.waive(x)) y else x
+}
+
+expand_default <- function(scale, discrete = c(0, 0.6), continuous = c(0.05, 0)) {
+  scale$expand %|W|% if (scale$is_discrete()) discrete else continuous
+}
+
+
 #' Split facet_wrap over multiple plots
 #'
 #' This extension to [ggplot2::facet_wrap()] will allow you to split
